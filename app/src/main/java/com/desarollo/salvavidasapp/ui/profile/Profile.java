adcf9ac18ec5dcbc40651ecb14651bbe909546d7
aperez900 <61300590+aperez900@users.noreply.github.com>
2021-03-11 00:42:47 -0500

    formulario perfil comprador (registro, actualizacion)

--- app/src/main/java/com/desarollo/salvavidasapp/ui/profile/Profile.java
+++ app/src/main/java/com/desarollo/salvavidasapp/ui/profile/Profile.java
@@ -1,8 +1,6 @@
 package com.desarollo.salvavidasapp.ui.profile;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,14 +8,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
+import com.desarollo.salvavidasapp.Home;
 import com.desarollo.salvavidasapp.R;
-import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -26,7 +28,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import static android.content.ContentValues.TAG;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 
 public class Profile extends Fragment {
@@ -42,12 +47,10 @@ public class Profile extends Fragment {
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
 
-        // Write a message to the database
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference("usuarios");
-        myRef.child(currentUser.getUid());
-
     }
+
 
     @Nullable
     @Override
@@ -55,19 +58,74 @@ public class Profile extends Fragment {
         //return super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_profile,container,false);
 
-        //Actualiza los datos del perfil logeado en el fragmenProfile
+
         TextView UserName = view.findViewById(R.id.nombre_perfil);
-        UserName.setText(currentUser.getDisplayName());
-
         TextView UserMail = view.findViewById(R.id.correo_perfil);
-        UserMail.setText(currentUser.getEmail());
-
         ImageView UserPhoto = view.findViewById(R.id.foto_perfil);
+        EditText nombres = view.findViewById(R.id.tv_nombre);
+        EditText apellidos = view.findViewById(R.id.tv_apellido);
+        EditText direccion = view.findViewById(R.id.tv_direccion);
+        EditText municipio = view.findViewById(R.id.tv_municipio);
+        EditText identificacion = view.findViewById(R.id.tv_identidad);
+        EditText celular = view.findViewById(R.id.tv_celular);
+        Button btn_reg = view.findViewById(R.id.btn_registrar_perfil);
+
+        //Actualiza los datos del perfil logeado en el fragmenProfile
+        UserName.setText(currentUser.getDisplayName());
+        UserMail.setText(currentUser.getEmail());
         Glide.with(this).load(currentUser.getPhotoUrl()).into(UserPhoto);
 
-        EditText primer_nombre = view.findViewById(R.id.tv_nombre);
-        primer_nombre.setText("Hola");
 
+        btn_reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               registrar(UserMail,nombres,apellidos,direccion,municipio,identificacion,celular);
+
+            }
+        });
         return view;
     }
+
+
+    private void registrar(TextView UserMail,EditText nombres, EditText apellidos, EditText direccion, EditText municipio, EditText identificacion, EditText celular) {
+
+        Map<String,Object> map= new HashMap<>();
+        map.put("nombre",nombres.getText().toString());
+        map.put("apellido",apellidos.getText().toString());
+        map.put("municipio",municipio.getText().toString());
+        map.put("direccion",direccion.getText().toString());
+        map.put("identificacion",identificacion.getText().toString());
+        map.put("celular",celular.getText().toString());
+
+        myRef.child(currentUser.getUid()).setValue(map)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        nombres.setText("");
+                        apellidos.setText("");
+                        municipio.setText("");
+                        direccion.setText("");
+                        identificacion.setText("");
+                        celular.setText("");
+
+                        Toast.makeText(getApplicationContext(), "Se actualizo de forma exitosa", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "No se actualizaron los datos, debido a un error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    //Consulta los datos del perfil del usuario logeado
+    public void consultarDatosPerfil(){
+
+
+    }
+
+
+
 }
