fe3a4a5ee49cfeb2ab97964c2c31adeaa8b54e29
aperez900 <61300590+aperez900@users.noreply.github.com>
2021-07-14 18:09:36 -0500

    se agrega valor en la pasarela de pagos

--- app/src/main/java/com/desarollo/salvavidasapp/ui/sales/buyProduct.java
+++ app/src/main/java/com/desarollo/salvavidasapp/ui/sales/buyProduct.java
@@ -88,7 +88,7 @@ public class buyProduct extends AppCompatActivity {
         btn_pago.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Uri uri = Uri.parse("https://checkout.wompi.co/p/?public-key=pub_test_KY4VrC344hkv91RHAfu9XRajobfm0ROe&currency=COP&amount-in-cents=10000&reference="+UUID.randomUUID().toString()+"&redirect-url=http%3A%2F%2Flocalhost%2FsalvavidasWeb%2F");
+                Uri uri = Uri.parse("https://checkout.wompi.co/p/?public-key=pub_test_KY4VrC344hkv91RHAfu9XRajobfm0ROe&currency=COP&amount-in-cents=1000000&reference="+UUID.randomUUID().toString()+"&redirect-url=http%3A%2F%2Flocalhost%2FsalvavidasWeb%2F");
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
             }
